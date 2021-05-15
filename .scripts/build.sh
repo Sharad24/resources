6697cb01e43d4a2052fbc299e06ff0306f2e6400
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2021-05-15 09:42:53 +0530

    Add local building script

---
+++ .scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf docs/
+set -x
+mkdir docs tmp
+for file in $(find _posts -type f -name '*.Rmd')
+	do
+	(\
+		diff -q {,tmp/}"${file}" && \
+			rsync -rv {tmp/,}$(dirname "${file}")/ \
+	) || \
+		Rscript -e "rmarkdown::render('${file}')"
+done
+rm -rf tmp/_posts
+cp -r _posts tmp
+
+Rscript -e 'rmarkdown::render_site(encoding = "UTF-8")'
